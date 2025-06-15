dc0a820603df8ad931fed7152803d0e7fcfd4550
yasiru123 <yasirulochanaperera@gmail.com>
2025-06-15 17:33:31 +0530

    remove axios, and change authstack

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -27,9 +27,9 @@ const AuthStack = () => {
       screenOptions={{
         headerShown: false,
       }}>
-              <Stack.Screen name="Register" component={RegisterScreen} />
+              
       <Stack.Screen name="Login" component={LoginScreen} />
-
+      <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Verification" component={VerificationScreen} />
       <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} />
     </Stack.Navigator>
